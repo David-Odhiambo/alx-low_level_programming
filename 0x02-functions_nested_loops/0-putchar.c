800858469c82698c186de616e59391b7b73595d0
David-Odhiambo <prof.odhiambodavid@gmail.com>
2022-04-13 10:55:07 +0300

    Write a function that checks for lowercase character

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,77 @@
+
+
+D
+D
+C
+C
+C
+
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+A
+A
+A
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+
+C
+
 #include "main.h"
 
+
 /**
- *  main - print main, followed by new line
- *  Return: 0
- */
+* main - Entry point
+*
+* Return: nothing, return void
+*/
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
-
 
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,8 @@
+#include "main.h
+A
+A
+A
+A
+A
+A
+"
