eb23ea0d97b18db64d07d6eba822cd3e95fd24dd
David-Odhiambo <prof.odhiambodavid@gmail.com>
2022-04-23 15:14:25 +0300

    Write a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,31 +10,26 @@
 char *cap_string(char *s)
 {
 	int i = 1, j, check;
-	char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
-
-	if (s[0] > 96 && s[0] < 123)
-		s[0] -= 32;
-
-	while (s[i] != '\0')
+char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
+        
+        if (s[0] > 96 && s[0] < 123)
 	{
-		if (s[i] > 96 && s[i] < 123)
+		j = 0;
+		check = 0;
+		while (check == 0 && j < 13)
 		{
-			j = 0;
-			check = 0;
-			while (check == 0 && j < 13)
+			if (s[i - 1] == a[j])
 			{
-				if (s[i - 1] == a[j])
-				{
-					check = 1;
-				}
-				j++;
+				check = 1;
 			}
-			if (check == 1)
-			{
-				s[i] -= 32;
-			}
+			j++;
 		}
-		i++;
+		if (check == 1)
+		{
+			s[i] -= 32;
+		}
 	}
-	return (s);
+	i++;
+}
+return (s);
 }
