bbbeb639747ca456ab078e67bbe567ff2a46e354
David-Odhiambo <prof.odhiambodavid@gmail.com>
2022-04-23 17:18:41 +0300

    Write a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *rot13 - encoding a string using rot13.
- *@s: the string to be encoded to rot13.
- *Return: the string s encoded to rot13.
+ * rot13 - encoding a string using rot13.
+ * @s: the string to be encoded to rot13.
+ * Return: the string s encoded to rot13.
  */
 
 char *rot13(char *s)
@@ -22,8 +22,7 @@ char *rot13(char *s)
 				break;
 			}
 			j++;
-		}
-		while (string_alpha[j] != '\0');
+		}while (string_alpha[j] != '\0');
 		j = 0;
 		i++;
 	}
