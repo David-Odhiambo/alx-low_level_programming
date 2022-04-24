691bd947e7da09c93941d18e993e353a8ce5c7d6
David-Odhiambo <prof.odhiambodavid@gmail.com>
2022-04-24 11:29:30 +0300

    Write a function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * infinite_add - add two numbers.
+ *infinite_add - add two numbers.
  *
- * @n1: first number.
- * @n2: second number.
- * @r: result.
- * @size_r: result size.
- * Return: the addition of n1 and n2.
+ *@n1: first number.
+ *@n2: second number.
+ *@r: result.
+ *@size_r: result size.
+ *Return: the addition of n1 and n2.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int add = 0, len1, len2, i, j;
-	for (len1 = 0; n1[len1]; len1++);
-	for (len2 = 0; n2[len2]; len2++);
+
+	for (len1 = 0; n1[len1]; len1++)
+	;
+	for (len2 = 0; n2[len2]; len2++)
+	;
 	if (len1 > size_r || len2 > size_r)
 		return (0);
 	len1--;
@@ -30,7 +33,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			break;
 		r[i] = add % 10 + '0';
 		add /= 10;
-
 	}
 	r[i] = '\0';
 	if (len1 >= 0 || len2 >= 0 || add)
@@ -43,3 +45,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	return (r);
 }
+
