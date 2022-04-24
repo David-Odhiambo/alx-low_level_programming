4b4eb14981049cf3779cc73042335612926a089e
David-Odhiambo <prof.odhiambodavid@gmail.com>
2022-04-24 12:48:15 +0300

    We are adding a line to a code so that the program prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,29 @@
+#include "main.h"
 #include <stdio.h>
 
-int main(void)
-{
-  int n;
-  int a[5];
-  int *p;
+/*
+ * main - add a new line to a code
+ * let program print a[2] = 98
+ * follow by a new line
+ */
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - only one statement
-   */
- p[5] = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+int main (void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 */
+	p[5] = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
