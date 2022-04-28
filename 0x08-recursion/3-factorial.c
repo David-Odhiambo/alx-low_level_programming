845850258443c4c9baf4ca10a25d56243f901e15
David-Odhiambo <prof.odhiambodavid@gmail.com>
2022-04-28 21:45:42 +0300

    returns the factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,10 +2,10 @@
 
 /**
  * factorial - gets factorial of n
- * @n: integer.
+ * @n: integer
  *
  * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int factorial(int n)
 {
@@ -13,5 +13,5 @@ int factorial(int n)
 		return (-1);
 	if (n == 0)
 		return (1);
-	return (n * factorial(n-1));
+	return (n * factorial(n - 1));
 }
